11564f706453a80ff6d7d954e7124e2c93159b47
gudaoxuri <i@sunisle.org>
2022-09-17 09:01:43 -0700

    Enhanced detection of develop tools status.

--- src/funs/disable_dev_tools.rs
+++ src/funs/disable_dev_tools.rs
@@ -25,17 +25,31 @@ fn disable_key() -> Result<(), JsValue> {
 
 fn check_has_special_funs() -> Result<(), JsValue> {
     let debugger_fun = js_sys::Function::new_no_args("debugger;");
+    let error_fun = js_sys::Function::new_no_args(
+        "console.log(Object.defineProperties(new Error, {
+  toString: {value() {(new Error).stack.includes('toString@')}},
+  message: {get() {document.body.setAttribute('_DT_','true')}},
+}));",
+    );
     let callback = Closure::wrap(Box::new(move || {
         web_sys::console::clear();
         let now = Date::now();
         debugger_fun.call0(&"".into()).unwrap();
+        error_fun.call0(&"".into()).unwrap();
         if Date::now() - now > 100f64 {
+            // test only : web_sys::window().unwrap().document().unwrap().get_element_by_id("develop_tools_status").unwrap().set_inner_html("develop tools opend");
+            has_error("DDT_DEBUG");
+            return;
+        }
+        if web_sys::window().unwrap().document().unwrap().body().unwrap().get_attribute("_DT_").is_some() {
+            // test only : web_sys::window().unwrap().document().unwrap().get_element_by_id("develop_tools_status").unwrap().set_inner_html("develop tools opend");
             has_error("DDT_DEBUG");
             return;
         }
     }) as Box<dyn Fn()>);
     web_sys::window().unwrap().set_interval_with_callback_and_timeout_and_arguments_0(callback.as_ref().unchecked_ref(), 1000).unwrap();
     callback.forget();
+
     Ok(())
 }
 
